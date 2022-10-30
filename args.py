17e2ec8b987be7d2c289327fcb666fc6432e7c00
Velho <jewel960@gmail.com>
2022-10-30 19:27:48 +0200

    Formatting changes.

--- args.py
+++ args.py
@@ -5,7 +5,9 @@ from argparse import ArgumentParser
 from queue import Empty
 
 DEFAULT_PROJECT_PATH = "C:\\Users\\petri\\c\\nginx\\"
-DEFAULT_OUTPUT_PATH = "C:\\Users\\petri\\python\\parse-function-names\\function_names.csv"
+DEFAULT_OUTPUT_PATH = (
+    "C:\\Users\\petri\\python\\parse-function-names\\function_names.csv"
+)
 VERSION = 0.1
 
 DEFAULT_BANNER = """
@@ -24,11 +26,11 @@ def welcome_banner():
     Welcome welcome welcome
     """
     print(f"{DEFAULT_BANNER}")
-    print(f"Welcome to mangler v.{VERSION}")
+    print(f"version {VERSION}")
 
 
 def arg_parser():
-    """ Public function
+    """Public function
     Initializes the parser and
     Returns the arguments if no arguments provided,
     return the help message.
@@ -43,22 +45,31 @@ def arg_parser():
 
 
 def init_parser():
-    """ Initializes the argument parser.
+    """Initializes the argument parser.
     Adds the default parser options,
     -p, --project as the project folder,
     -o, --output as the output csv filename,
     """
 
     parser = ArgumentParser()
-    parser.add_argument('--project', '-p', help='Input project folder.',
-                        default=DEFAULT_PROJECT_PATH, type=str)
     parser.add_argument(
-        '--output', '-o', help='Output for the csv, provide the whole folder/file.csv.', default=DEFAULT_OUTPUT_PATH, type=str)
+        "--project",
+        "-p",
+        help="Input project folder.",
+        default=DEFAULT_PROJECT_PATH,
+        type=str,
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        help="Output for the csv, provide the whole folder/file.csv.",
+        default=DEFAULT_OUTPUT_PATH,
+        type=str,
+    )
 
     return parser
 
 
 def get_args(parser):
-    """ Returns the parsed arguments.
-    """
+    """Returns the parsed arguments."""
     return parser.parse_args()
